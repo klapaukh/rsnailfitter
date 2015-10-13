4ce2787e4e28b13d8f00eb5e6ea1a1a7facca661
Roma Klapaukh <rklapaukh@hotmail.com>
2015-10-13 20:58:20 +1300

    Add in better documentation

--- R/estimate.R
+++ R/estimate.R
@@ -1,18 +1,43 @@
-#' @importFrom magrittr "%<>%"
-
+#' Calculate W based on a measured increase in the radius of a snail. Note that
+#' this assumes that the measurements are of the centroid of the snail shell.
+#' 
+#' @param theta The angle across which the measuring has been done in radians
+#' @param r_theta The distance from the coiling axis at the end of the measured
+#'        section
+#' @param r_0 The distance from the coiling axis the start of the measured 
+#'        section
+#' @export
 estimateW <- function(theta, r_theta, r_0) {
   (r_theta/r_0) ^ ( (2*pi) / theta )
 }
 
+#' Calculate T based on a measured section of snail (where W has already been
+#' estimated). This uses two points measured on a snail. Note that this assumes
+#' that the measurements are of the centroid of the snail shell.
+#'
+#' @param theta The angle difference between the two measured points in radians
+#' @param y_theta The difference in y between the two measured points
+#' @param r_0 The distance from the coilind axis at the first point
+#' @param W The calculated W value for the spiral
+#' @export
 estimateT <- function(theta, y_theta, r_0, W){
   y_theta / ( r_0 * ( (W ^ (theta / (2*pi))) - 1 ))
 }
 
-#' Estimate W and T based on every pair of points.
+#' Estimate W and T based on every pair of points. Since W and T can be
+#' estimated from any sub part of the shell, this estimates W and T based on
+#' every possible point being an end point. For each end point, every theta
+#' value smaller than it acts as a starting point. 
+#'
+#' The data frame that is used in this method must have three columns named
+#' 'theta', 'r', and 'y'. The theta column must be measured in radians. The
+#' other measurements can be in any set of units as long as they are all the
+#' same. 
 #' 
 #' @param dt The data frame containing snail measurements
 #' @return A list of data.frames containing W and T estimates for every pair of
-#'         thetas.
+#'         thetas. Each list element is named according the final point it used.
+#' @export
 #' @examples
 #' estimateOverTime(snail)
 estimateOverTime <- function(dt){
---
+++ R/rsnailfitter.R
@@ -0,0 +1,12 @@
+#' rsnailfitter is a package aimed at estimating Raupian parameters based on
+#' measured snails. All angles must be measured in radians, but distances can be
+#' measured in any units as long as they are all the same. Note, that unlike the
+#' description in Raup's original paper
+#'
+#' @references Geometric Analysis of Shell Coiling: General Problems (1966).
+#'             David M. Raup. Journal of Paleontology, vol 40, No 5, pp 
+#'             1178-1190
+#' @docType package
+#' @name rsnailfitter
+#' @importFrom magrittr "%<>%"
+NULL
